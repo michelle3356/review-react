d3a1a96c55640465de204e58667df8232f4f723d
michelle3356 <michelle3356@yahoo.com.tw>
2020-09-15 00:17:54 +0800

    hw1 counter

---
+++ src/Counter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+class Counter extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      counterNum: 0
+    }
+  };
+
+  handleClick = (counterNum) => {
+    console.log(counterNum)
+    this.setState({
+      counterNum: this.state.counterNum + 1
+    })
+  }
+
+
+  render(){
+    var counterNum = this.state.counterNum;
+    return(
+      <div>
+        <h1>計數器</h1>
+        <button
+        onClick={this.handleClick.bind(this)}>click,than num = {counterNum}</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
