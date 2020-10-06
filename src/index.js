b206a10184693b3d1c20db5b2a5087a9defbc53f
michelle3356 <michelle3356@yahoo.com.tw>
2020-10-07 01:54:46 +0800

    router success

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import H1Counter from './H1Counter';
-import H2CounterList from './H2CounterList';
-import H3CounterSum from './H3CounterSum';
-import H4BMI from './H4BMI';
-import H5ItemList from './H5ItemList';
-import H6RemoveList from './H6RemoveList';
 import * as serviceWorker from './serviceWorker';
 import HomeRoute from './HomeRoute';
-import Home from './Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <HomeRoute />
   </React.StrictMode>,
   document.getElementById('root')
 );
