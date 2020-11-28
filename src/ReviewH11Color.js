3cdf5e3fa10c36e266fcfab3af4a2299f4498f74
michelle3356 <michelle3356@yahoo.com.tw>
2020-11-28 17:38:04 +0800

    review color

--- src/ReviewH11Color.js
+++ src/ReviewH11Color.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import ReviewH11ColorInput from './ReviewH11ColorInput';
+
 
 class ReviewH11Color extends React.Component{
   constructor(props){
     super(props);
-
     this.state = {
       r: 0,
       g: 0,
@@ -11,15 +12,25 @@ class ReviewH11Color extends React.Component{
     }
   }
 
+  handleChangeColor = (colorRange, value) => {
+    // console.log(colorRange, value)
+    this.setState({
+      [colorRange]: value,
+    })
+  }
+
   render(){
 
     const {r, g, b} = this.state;
+
     return(
       <div>
         <h1>調色盤</h1>
         <h2>用input type="range",製作拉桿</h2>
-        <input type="range" />
-
+        <ReviewH11ColorInput colorRange="r" onChangeColor={this.handleChangeColor} colorValue={r} />
+        <ReviewH11ColorInput colorRange="g" onChangeColor={this.handleChangeColor} colorValue={g} />
+        <ReviewH11ColorInput colorRange="b" onChangeColor={this.handleChangeColor} colorValue={b} />
+        <div style={{width: 100, height: 100, background: `rgb(${r}, ${g}, ${b}`}}></div>
       </div>
     )
   }
--- src/ReviewH11ColorInput.js
+++ src/ReviewH11ColorInput.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-
+const Name = {
+  r: 'Red',
+  g: 'Green',
+  b: 'Blue'
+}
 class ReviewH11ColorInput extends React.Component{
 
+
+  handleChange = (e) => {
+    this.props.onChangeColor(this.props.colorRange, e.target.value);
+  }
+
   render(){
-    const value = this.props;
+    const {colorValue, colorRange} = this.props;
 
     return(
       <div>
+        <label>{Name[colorRange]}</label>
         <input
-        type="range"
-        min="0"
-        max="255"
-        step="1" />
+          type="range"
+          min="0"
+          max="255"
+          step="1"
+          value={colorValue}
+          onChange={this.handleChange.bind(this)} />
       </div>
     )
   }
